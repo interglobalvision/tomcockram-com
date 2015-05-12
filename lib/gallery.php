1e94a3b0fea2f7b7793d971389be09f5c364e2a3
mikey <michaelrayvon@gmail.com>
2015-05-12 18:50:38 -0500

    remove extra crap

--- lib/gallery.php
+++ lib/gallery.php
@@ -29,10 +29,6 @@ function my_gallery_shortcode($attr) {
 		'order'      => 'ASC',
 		'orderby'    => 'menu_order ID',
 		'id'         => $post->ID,
-		'itemtag'    => 'li',
-		'icontag'    => 'li',
-		'captiontag' => 'span',
-		'columns'    => 3,
 		'size'       => 'gallery',
 		'include'    => '',
 		'exclude'    => ''
@@ -65,64 +61,24 @@ function my_gallery_shortcode($attr) {
 		return $output;
 	}
 
-	$itemtag = tag_escape($itemtag);
-	$captiontag = tag_escape($captiontag);
-	$icontag = tag_escape($icontag);
-	$valid_tags = wp_kses_allowed_html( 'post' );
-	if ( ! isset( $valid_tags[ $itemtag ] ) )
-		$itemtag = 'dl';
-	if ( ! isset( $valid_tags[ $captiontag ] ) )
-		$captiontag = 'dd';
-	if ( ! isset( $valid_tags[ $icontag ] ) )
-		$icontag = 'dt';
-
-	$columns = intval($columns);
-	$itemwidth = $columns > 0 ? floor(100/$columns) : 100;
-	$float = is_rtl() ? 'right' : 'left';
-
 	$selector = "gallery-{$instance}";
 
-	$gallery_div = "<div id='$selector' class='cycle-slideshow gallery galleryid-{$id}' data-cycle-fx='scrollHorz' data-cycle-timeout='0' data-cycle-swipe=true data-cycle-slides='div'>
-	<nav class='cycle-prev'></nav>
-    <nav class='cycle-next'></nav>
-    ";
+	$gallery_div = "<div id='$selector' class='gallery galleryid-{$id}' data-cycle-fx='scrollHorz'>";
+	// data-cycle-timeout='0' data-cycle-swipe=true data-cycle-slides='div'
 	$output = $gallery_div;
 
 	$i = 0;
 	foreach ( $attachments as $id => $attachment ) {
 
-		$tag = '';
-
 		$img = wp_get_attachment_image_src($id, $size);
-/*
-		$largeimg = wp_get_attachment_image_src($id, 'single');
-		$large = $largeimg[0];
-*/
 
-if ( $captiontag && trim($attachment->post_excerpt) ) {
-			$tag = "
-				<{$captiontag} class='wp-caption-text gallery-caption'>
-				" . wptexturize($attachment->post_excerpt) . "
-				</{$captiontag}>";
+		if ( trim($attachment->post_excerpt) ) {
+			$tag = " alt='" . wptexturize($attachment->post_excerpt) . "'";
 		} else {
 			$tag = null;
 		}
 
-		/*
-
-		$output .= "
-			<{$icontag} class='gallery-item'>
-			<div class='gallery-item-holder'>
-				<div class='gallery-img'>
-					<img src='{$img[0]}'>
-				</div>
-				{$tag}
-			</div>
-			</{$icontag}>";
-*/
-
-
-		$output .= "<div><img src='{$img[0]}'>{$tag}</div>";
+		$output .= "<div><img src='{$img[0]}'{tag}></div>";
 		}
 
 	$output .= "</div>\n";
