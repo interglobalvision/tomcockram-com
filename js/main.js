af6dd2f31c90d7bb7af97826e0206aaf9b3855d8
Patrick Best <patrickbest@patrickbest.com>
2015-05-20 17:20:27 -0500

    code review js & edit gulp for jscs

--- js/main.js
+++ js/main.js
@@ -7,10 +7,13 @@ function l(data) {
 }
 
 var
-	winHeight = $(window).height(),
+  winHeight = $(window).height(),
+  // >>> imo winHeight is still too shorthand. Better windowHeight for clarity
   animSpeed = 300,
+  // >>> this too
   slideMargin = parseInt( $('#header').css('padding-top'), 10 ),
   slideIndex,
+  // this slideIndex var is never used
   headerTop = $('#header').offset().top;
 
 jQuery(document).ready(function () {
@@ -19,22 +22,23 @@ jQuery(document).ready(function () {
 
   // SPLASH
   if ($('#splash').length) {
-  	$('.splash-margin').css('margin-top', winHeight);
-  	$('#main-container').removeClass('u-hidden');
+    $('.splash-margin').css('margin-top', winHeight);
+    $('#main-container').removeClass('u-hidden');
     $(window).on('scroll', function() {
       if ($(window).scrollTop() >= $('#main-container').offset().top && $('#splash').length) {
         $('#splash').remove();
         $('.splash-margin').css('margin-top', 0);
-        $(window).scrollTop(0)
+        $(window).scrollTop(0);
       }
     });
   }
 
+// >>> put this binding inside the if for perf
   $('#splash').on('click', function() {
     $('html, body').animate({ scrollTop: $('#main-container').offset().top }, ( animSpeed * 2 ));
   });
 
-  // PACKERY
+  // MASONRY
   if ( $('.js-masonry-container').length ) {
     $('.js-masonry-container').imagesLoaded( function() {
       $('.js-masonry-container').masonry({
@@ -43,16 +47,18 @@ jQuery(document).ready(function () {
         gutter: 0,
         transitionDuration: '0s',
       }).css({
-        'opacity': 1
+        'opacity': 1,
       });
     });
   }
 
   //SLIDER
+  // >>> hoist all variable to top
   var headerHeight = $('#header').outerHeight();
   var sliderHeight = winHeight - headerHeight - slideMargin;
 
   if ( $('.slider-content').length ) {
+    // >>> this isnt actually working until a window resize. on load this doesnt fire/set the correct value
     $('.slider-img').css('max-height', sliderHeight );
   }
 
@@ -60,6 +66,7 @@ jQuery(document).ready(function () {
     click: function() {
       if ($('.slider-content').hasClass('slider-hidden')) {
         var slideIndex = $(this).attr('data-index');
+
         $('.cycle-slideshow').cycle('goto', slideIndex);
         $('.masonry-content').animate({'opacity' : 0}, animSpeed, function() {
           $('.masonry-content').css('display','none');
@@ -73,7 +80,7 @@ jQuery(document).ready(function () {
   $('#view-toggle').on({
     click: function() {
       $('.slider-content').animate({'opacity' : 0}, animSpeed, function() {
-        $('.masonry-content').css('display','block')
+        $('.masonry-content').css('display','block');
         $('.js-masonry-container').masonry();
         $('.masonry-content').animate({'opacity' : 1}, animSpeed);
         $('.slider-content').addClass('slider-hidden');
@@ -82,8 +89,9 @@ jQuery(document).ready(function () {
     }
   });
 
+  // >>> there are a lot of jquery selectors here that need to be cached. but yes we wait til after ajaxy or not
 
-  //STICK HEADER
+  // STICK HEADER
   var $headerSpacer = $('.js-header-spacer');
 
   if ($headerSpacer.length) {
@@ -100,6 +108,7 @@ jQuery(document).ready(function () {
 
       if ($('#header').hasClass('u-fixed')) {
         if ($(window).scrollTop() < headerTop) {
+          // >>> these double if's could be joined with &&
           $('#header').removeClass('u-fixed');
           $headerSpacer.addClass('u-hidden');
         }
@@ -113,21 +122,20 @@ jQuery(document).ready(function () {
     });
   }
 
-
-  //RESIZE
+  // RESIZE
   $(window).on('resize', function() {
-  	winHeight = $(window).height();
+    winHeight = $(window).height();
     headerHeight = $('#header').outerHeight();
     slideMargin = parseInt( $('#header').css('padding-top'), 10 );
     sliderHeight = winHeight - headerHeight - slideMargin;
 
-		if ($('#splash').length) {
-	  	$('.splash-margin').css('margin-top', winHeight);
-	  }
+    if ($('#splash').length) {
+      $('.splash-margin').css('margin-top', winHeight);
+    }
 
     if ( $('.slider-content').length ) {
       $('.slider-img').css('height', sliderHeight );
     }
-  })
-  
+  });
+
 });
