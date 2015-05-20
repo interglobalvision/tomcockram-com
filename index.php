3c7ca0e3c7f5bc964323eed253cfcf9818dcf142
mikey <michaelrayvon@gmail.com>
2015-05-20 11:38:21 -0500

    update container structure

--- index.php
+++ index.php
@@ -8,43 +8,49 @@ get_header();
 
   <!-- main posts loop -->
   <section id="posts">
+    <div class="container">
+      <div class="row">
 
 <?php
 if( have_posts() ) {
 ?>
-    <div class="js-masonry-container">
+        <div class="js-masonry-container">
 <?php
   while( have_posts() ) {
     the_post();
 ?>
+          <div class="js-masonry-item feed-item col">
+            <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+              <a href="<?php the_permalink() ?>">
 
-      <div class="js-masonry-item feed-item col">
-        <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-          <a href="<?php the_permalink() ?>">
-
-            <div class="feed-item-title">
-              <div class="u-holder">
-                <div class="u-held">
-                  <h2 class="u-align-center"><?php the_title(); ?></h2>
+                <div class="feed-item-title">
+                  <div class="u-holder">
+                    <div class="u-held">
+                      <h2 class="u-align-center"><?php the_title(); ?></h2>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
 
-            <?php the_post_thumbnail( 'medium', 'class=feed-item-img'); ?>
+                <?php the_post_thumbnail( 'medium', 'class=feed-item-img'); ?>
 
-          </a>
-        </article>
-      </div>
+              </a>
+            </article>
+          </div>
 
 <?php } // END WHILE ?>
-  </div>
+        </div>
 <?php 
 } else {
 ?>
-    <article class="u-alert"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
+        <div class="col col12">
+          <article class="u-alert"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
+        </div>
+      
 <?php
 } ?>
 
+      </div>
+    </div>
   <!-- end posts -->
   </section>
 
