d88d674ac27960c6a79e98db00e178f05a8a8d4c
Patrick Best <patrickbest@patrickbest.com>
2015-06-02 14:46:06 -0500

    add base styling for pagination

--- partials/pagination.php
+++ partials/pagination.php
@@ -2,7 +2,9 @@
 if( get_next_posts_link() || get_previous_posts_link() ) {
 ?>
   <!-- post pagination -->
-  <nav id="pagination">
+  <nav id="pagination" class="container">
+    <div class="row">
+      <div class="col col12 u-align-center">
 <?php
 $previous = get_previous_posts_link('Newer');
 $next = get_next_posts_link('Older');
@@ -16,6 +18,8 @@ if ($next) {
   echo $next;
 }
 ?>
+      </div>
+    </div>
   </nav>
 <?php
 }
