269bade2e9965e80d1591c3913b1d9167b58f448
Patrick Best <patrickbest@patrickbest.com>
2015-06-09 15:04:18 -0500

    fix overlay text hovers for large screens

--- index.php
+++ index.php
@@ -23,31 +23,30 @@ if( have_posts() ) {
           <div class="js-masonry-item feed-item col" style="width: <?php echo mt_rand(15, 40); ?>%; margin-bottom: <?php echo mt_rand(50, 250); ?>px; margin-right: <?php echo mt_rand(30, 60); ?>px;">
             <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
               <a href="<?php the_permalink() ?>">
-
-                <div class="feed-item-title">
-                  <div class="u-holder">
-                    <div class="u-held">
-                      <h2 class="u-align-center"><?php the_title(); ?></h2>
+                <div class="feed-item-holder">
+                  <div class="feed-item-title">
+                    <div class="u-holder">
+                      <div class="u-held">
+                        <h2 class="u-align-center"><?php the_title(); ?></h2>
+                      </div>
                     </div>
                   </div>
-                </div>
 
 <?php
   if (!empty($meta['_igv_webm'][0]) && !empty($meta['_igv_mp4'][0])) {
     $thumb_img = wp_get_attachment_image_src(get_post_thumbnail_id(), 'index-thumb');
 ?>
-                <video autoplay loop poster="<?php echo $thumb_img[0]; ?>">
-                  <source src="<?php echo $meta['_igv_webm'][0]; ?>" type='video/webm'/>
-                  <source src="<?php echo $meta['_igv_mp4'][0]; ?>" type='video/mp4'/>
-                </video>
+                  <video autoplay loop poster="<?php echo $thumb_img[0]; ?>">
+                    <source src="<?php echo $meta['_igv_webm'][0]; ?>" type='video/webm'/>
+                    <source src="<?php echo $meta['_igv_mp4'][0]; ?>" type='video/mp4'/>
+                  </video>
 <?php
   } else {
   the_post_thumbnail( 'index-thumb', 'class=feed-item-img');
 
   }
 ?>
-<!--            >>> should be use custom image size here? should we use lazyloading technique from master theme? -->
-
+                </div>
               </a>
             </article>
           </div>
